9389b9d237cdfd201add9616f9cdcf77dc38e155
Alejandro Sanchez Pinto <alejandrosanchezpinto@gmail.com>
2025-05-10 10:25:19 +0200

    Added Lombook dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    kotlin("plugin.lombok") version "2.1.20"
+    id("io.freefair.lombok") version "8.13"
 }

16400654b41e0d3e7e4995e0686dea75ba7372e8
Alejandro Sanchez Pinto <alejandrosanchezpinto@gmail.com>
2025-05-19 23:33:50 +0200

    Update package name in native distribution settings and ensure icon file is set for all platforms

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,16 +37,21 @@ kotlin {
 compose.desktop {
     application {
         mainClass = "org.queststudios.projecttask.MainKt"
-        // Set the application icon (place icon.png in composeApp/src/desktopMain/resources)
         nativeDistributions {
             targetFormats(TargetFormat.Exe, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Dmg)
-            packageName = "ProjectTask"
+            packageName = "Project Task"
             packageVersion = "1.0.0"
             windows {
-                // Optional: configure Windows installer
                 menuGroup = "ProjectTask"
                 dirChooser = true
                 shortcut = true
+                iconFile.set(project.file("src/desktopMain/resources/icon.png"))
+            }
+            macOS {
+                iconFile.set(project.file("src/desktopMain/resources/icon.png"))
+            }
+            linux {
+                iconFile.set(project.file("src/desktopMain/resources/icon.png"))
             }
         }
     }
